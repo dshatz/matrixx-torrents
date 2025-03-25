37c843b723d094ded6233c808e38d86ff89c1af0
Daniels Šatcs <dev@dshatz.com>
2025-03-25 20:33:37 +0100

    Run locally

--- script.py
+++ script.py
@@ -19,7 +19,7 @@ def fetch_feed():
 def find_new_release(entries):
     print("Looking for releases")
     for entry in entries:
-        print(entry)
+        print(entry.title)
         if "Official-" + KEYWORD in entry.title.lower():
             return entry.link
     return None
